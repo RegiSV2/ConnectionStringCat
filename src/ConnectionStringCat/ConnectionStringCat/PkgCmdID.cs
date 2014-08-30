a8126d5b686b9e2ec334d014602a744d4ed5746b
RegiSV2 <sserregiss@gmail.com>
2014-08-31 00:45:19 +0400

    Main files

---
+++ src/ConnectionStringCat/ConnectionStringCat/PkgCmdID.cs
@@ -0,0 +1,15 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace SergeyUskov.ConnectionStringCat
+{
+	static class PkgCmdIdList
+	{
+		public const uint SetupConStringsCmdId = 0x100;
+
+		public const uint ConnectionStringsCombo = 0x101;
+
+		public const uint ConnectionStringsListId = 0x102;
+	};
+}
