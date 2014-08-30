d17beef5b1926e1aba379e7703e5690aa0bd811c
RegiSV2 <sserregiss@gmail.com>
2014-08-30 21:27:48 +0400

    Initial commit

---
+++ src/ConnectionStringCat/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace SergeyUskov.ConnectionStringCat
+{
+    static class GuidList
+    {
+        public const string guidConnectionStringCatPkgString = "bf2c1368-abc7-4dd2-a198-8374f7a26974";
+        public const string guidConnectionStringCatCmdSetString = "9bebfb67-4d8e-4275-b3a1-f29a81be5e2e";
+
+        public static readonly Guid guidConnectionStringCatCmdSet = new Guid(guidConnectionStringCatCmdSetString);
+    };
+}
---
+++ src/ConnectionStringCat/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConnectionStringCat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sergey Uskov")]
+[assembly: AssemblyProduct("ConnectionStringCat")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("ConnectionStringCat_IntegrationTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001006f4f7bf6e76908729e1ee7459026e99e651102b0629a17daaed7e7ba5f9704d4feb37b5e55ff6b92b6c2380437a0ca74f572f2abb749c18bb05d94790d10512707bf730767ee730c9128a1e5d0030d8f07a7eb5557fdd0983160cbed769196146cfb0a6d3786e92cd533eeca86bdc8dfaa555487f7af68cb6050296442f0f7b3")]
+[assembly: InternalsVisibleTo("ConnectionStringCat_UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001006f4f7bf6e76908729e1ee7459026e99e651102b0629a17daaed7e7ba5f9704d4feb37b5e55ff6b92b6c2380437a0ca74f572f2abb749c18bb05d94790d10512707bf730767ee730c9128a1e5d0030d8f07a7eb5557fdd0983160cbed769196146cfb0a6d3786e92cd533eeca86bdc8dfaa555487f7af68cb6050296442f0f7b3")]
